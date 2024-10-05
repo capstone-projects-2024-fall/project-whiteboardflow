45822dea873c356811bd7980fd0e0026dc6704ea
VinceLukban <112022481+VinceLukban@users.noreply.github.com>
2024-10-05 11:37:21 -0400

    recommit

---
+++ whiteboardflow-frontend/src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import App from './App';  
+import Login from './Login'; 
+
+function Main() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Navigate to="/login" replace />} /> 
+            </Routes>
+        </Router>
+    );
+}
+
+export default Main;
