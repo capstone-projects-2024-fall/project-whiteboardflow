707bdf03745ae1d60e11b60904eccb233263087d
Dean Roos <tug91912@temple.edu>
2024-10-07 10:26:39 -0400

    Screen Shot POC

--- whiteboardflow-frontend/src/Main.js
+++ whiteboardflow-frontend/src/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage.js';  // Import the new HomePage component
 import Login from './Login.js';  // Import the Login component
+import ScreenShotTest from './ScreenShotTest.js';
 
 function Main() {
     return (
@@ -9,6 +10,7 @@ function Main() {
             <Routes>
                 <Route path="/" element={<HomePage />} />  {/* Home page route */}
                 <Route path="/login" element={<Login />} />  {/* Login page route */}
+                <Route path="/sstest" element={<ScreenShotTest />} />
             </Routes>
         </Router>
     );
