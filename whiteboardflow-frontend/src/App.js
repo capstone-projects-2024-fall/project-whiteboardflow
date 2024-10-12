3be88df9228aa088452e77390e62a23618608c55
Edwardd02 <tup19197@temple.edu>
2024-10-11 22:05:30 -0400

    Whiteboard is working properly now

--- whiteboardflow-frontend/src/App.js
+++ whiteboardflow-frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div>
             <h1>Welcome to the App</h1>
-            
+
         </div>
     );
 }
--- whiteboardflow-frontend/src/Main.js
+++ whiteboardflow-frontend/src/Main.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import App from './App';  
+import App from './App';
 import Login from './pages/Login/Login';
+import Whiteboard from './components/Whiteboard/Whiteboard';
 
 function Main() {
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                <Route path="/" element={<Navigate to="/login" replace />} /> 
+                <Route path="/whiteboard" element={<Whiteboard />} />
+                <Route path="/" element={<Navigate to="/login" replace />} />
             </Routes>
         </Router>
     );
