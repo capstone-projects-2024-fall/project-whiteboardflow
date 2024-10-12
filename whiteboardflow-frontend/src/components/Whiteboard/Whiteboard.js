0a9c097fff3587a00e8d20e86c9395cf6b23a58a
VinceLukban <112022481+VinceLukban@users.noreply.github.com>
2024-10-12 19:25:23 -0400

    created test for Whiteboard.js

--- whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
+++ whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
@@ -1,39 +1,25 @@
+import React from 'react';
+
+/** Draws on the whiteboard. */
+export const draw = () => { };
+
+/** Erases part of the whiteboard. */
+export const erase = () => { };
+
+/** Undoes the last action on the whiteboard. */
+export const undo = () => { };
+
+/** Redoes the last undone action on the whiteboard. */
+export const redo = () => { };
+
 /**
  * Whiteboard available for the user during a written test.
  * @component
  */
 const Whiteboard = () => {
-
-  /**
-   * Draws on the whiteboard.
-   */
-  const draw = () => {
-
-  };
-
-  /**
-   * Erases part of the whiteboard.
-   */
-  const erase = () => {
-
-  };
-
-  /**
-   * Undoes the last action on the whiteboard.
-   */
-  const undo = () => {
-
-  };
-
-  /**
-   * Redoes the last undone action on the whiteboard.
-   */
-  const redo = () => {
-
-  };
-
   return (
-    <div>
+    <div role="region" aria-label="whiteboard">
+      {/* Whiteboard content */}
     </div>
   );
 };
