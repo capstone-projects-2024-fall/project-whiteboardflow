b400f5707b5002d199dcfb55c47ec95b3c9115ab
VinceLukban <112022481+VinceLukban@users.noreply.github.com>
2024-10-13 14:05:26 -0400

    updating Whiteboard

--- whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
+++ whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
@@ -1,7 +1,21 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
+//import React, { useEffect, useRef } from 'react';
 import './css/reset.css';
 import './css/components.css';
 import './css/examples.css';
+/** Draws on the whiteboard. */
+export const draw = () => { };
+
+/** Erases part of the whiteboard. */
+export const erase = () => { };
+
+/** Undoes the last action on the whiteboard. */
+export const undo = () => { };
+
+/** Redoes the last undone action on the whiteboard. */
+export const redo = () => { };
+
+
 
 const Whiteboard = () => {
   const editorElementRef = useRef(null);
