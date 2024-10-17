0ca6e122c48d185650ca05ac4961a2a790a5e858
bstephe9 <tun78906@temple.edu>
2024-10-17 17:23:29 -0400

    Add voice button

--- whiteboardflow-frontend/src/pages/OralTest/OralTest.js
+++ whiteboardflow-frontend/src/pages/OralTest/OralTest.js
@@ -1,11 +1,13 @@
 import React from 'react';
-//import HomePage from './HomePage.js';
-
+import { Button } from '@mui/material';
 
 function OralTest() {
     return (
         <div>
             <h1>Welcome to the Oral practice!</h1>
+            <Button variant="contained" onClick={null}>
+                Record
+            </Button>
         </div>
     );
 }
