2f30aa6e469c0383e3d3ff844489c19dd9fda356
bstephe9 <tun78906@temple.edu>
2024-10-17 17:56:02 -0400

    Add frontend button functionality and define record route

--- whiteboardflow-frontend/src/pages/OralTest/OralTest.js
+++ whiteboardflow-frontend/src/pages/OralTest/OralTest.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Button } from '@mui/material';
 
 function OralTest() {
+    const handleButtonClick = () => {
+        fetch('api/record')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                alert(data.message);
+          })
+            .catch((error) => {
+                console.error('Error:', error);
+          });
+    }
+
     return (
         <div>
             <h1>Welcome to the Oral practice!</h1>
-            <Button variant="contained" onClick={null}>
+            <Button variant="contained" onClick={handleButtonClick}>
                 Record
             </Button>
         </div>
