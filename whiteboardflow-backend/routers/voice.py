024dba982e79087856b0855600813cbe41ef7188
bstephe9 <tun78906@temple.edu>
2024-10-18 10:28:54 -0400

    2 Seconds

--- whiteboardflow-backend/routers/voice.py
+++ whiteboardflow-backend/routers/voice.py
@@ -5,12 +5,12 @@ from fastapi import APIRouter
 router = APIRouter()
 
 recognizer = sr.Recognizer()
-
+recognizer.energy_threshold = 0
 
 @router.get("/record/")
 def record():
     with sr.Microphone() as source:
-        audio = recognizer.listen(source)
+        audio = recognizer.listen(source, phrase_time_limit=2)
     try:
         text = recognizer.recognize_google(audio)
     except sr.UnknownValueError:
