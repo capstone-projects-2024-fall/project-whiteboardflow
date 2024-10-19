73f730a668da6cc1675e732fd0817d075c65e0e6
Edwardd02 <tup19197@temple.edu>
2024-10-19 15:22:50 -0400

    some clean ups

--- whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
+++ whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
@@ -54,52 +54,14 @@ const Whiteboard = () => {
             window.removeEventListener("resize", handleResize);
         };
     }, []);
+
     const handleExportAndSubmit = async () => {
         try {
-        // Assuming downloadAsPNG returns a Blob directly; adjust based on actual return type
-            const blob = await editor.behaviors.sendSVGToServer(0);
-
+            await editor.behaviors.sendSVGToServer(0);
         } catch (error) {
             console.error('Error during export or send:', error);
         }
     };
-    function customDownloadAsPNG() {
-        const r = editor.model.symbols; // Assuming all symbols should be used
-        const s = editor.getSymbolsBounds(r);
-        const n = editor.buildBlobFromSymbols(r, s);
-        const a = URL.createObjectURL(n);
-        const l = new Image();
-        l.width = s.width;
-        l.height = s.height;
-        l.src = a;
-        l.onload = async () => {
-            const canvas = document.createElement("canvas");
-            canvas.width = l.width;
-            canvas.height = l.height;
-            const context = canvas.getContext("2d");
-            context.drawImage(l, 0, 0);
-            URL.revokeObjectURL(a);
-            const imageBase64 = canvas.toDataURL("image/png");
-
-            // Instead of downloading, send this to the server
-            try {
-                const response = await fetch('/api/upload-image', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ image: imageBase64 })
-                });
-                if (response.ok) {
-                    console.log('Image successfully uploaded to the server');
-                } else {
-                    throw new Error('Failed to upload image');
-                }
-            } catch (error) {
-                console.error('Error uploading image:', error);
-            }
-        };
-    }
 
     return (
         <div style={{width: '100%', height: '90vh', position: 'relative'}}>
