c6b5d341b40ee28f0ade7a492f4e8c9c18ef31a6
dhruvilpatel999 <tup31383@temple.edu>
2024-10-27 22:53:42 -0400

    White board submit button

--- whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
+++ whiteboardflow-frontend/src/components/Whiteboard/Whiteboard.js
@@ -1,21 +1,20 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import HelpButton from './HelpButton';
 import SubmitButton from './SubmitButton';
 import './css/reset.css';
 import './css/components.css';
 import './css/examples.css';
 
-
-
-
 const Whiteboard = () => {
     const editorElementRef = useRef(null);
-    let editor = null;
+    const editorRef = useRef(null); // Use useRef to store the editor instance
+    const navigate = useNavigate();
 
     useEffect(() => {
         const handleResize = () => {
-            if (editor) {
-                editor.resize();
+            if (editorRef.current) {
+                editorRef.current.resize();
             }
         };
 
@@ -41,9 +40,9 @@ const Whiteboard = () => {
                         }
                     },
                 };
-                editor = new window.iink.Editor(editorElementRef.current, options);
-                await editor.initialize();
-                if (!editor) {
+                editorRef.current = new window.iink.Editor(editorElementRef.current, options);
+                await editorRef.current.initialize();
+                if (!editorRef.current) {
                     console.error("Editor is not initialized.");
                 }
                 window.addEventListener("resize", handleResize);
@@ -60,7 +59,10 @@ const Whiteboard = () => {
 
     const handleExportAndSubmit = async () => {
         try {
-            await editor.behaviors.sendPNGToServer(editor.behaviors.haveSymbolsSelected);
+            if (editorRef.current) {
+                await editorRef.current.behaviors.sendPNGToServer(editorRef.current.behaviors.haveSymbolsSelected);
+                navigate('/oraltest'); // Navigate to the OralTest page after export
+            }
         } catch (error) {
             console.error('Error during export or send:', error);
         }
