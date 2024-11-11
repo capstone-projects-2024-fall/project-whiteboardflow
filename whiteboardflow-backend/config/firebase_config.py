c4fad330da5856bd65b9507db66dccebbb6de218
Dean Roos <tug91912@temple.edu>
2024-11-11 08:53:50 -0500

    margin change

--- whiteboardflow-backend/config/firebase_config.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-
-from config.settings import Config
-
-cred = firebase_admin.credentials.Certificate(Config.FIREBASE_SERVICE_ACCOUNT_PATH)
-firebase_app = firebase_admin.initialize_app(
-    cred, {"storageBucket": Config.FIREBASE_STORAGE_BUCKET}
-)
