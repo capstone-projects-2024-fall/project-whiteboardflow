63f6b13f010d76578c6383bcf7e764912a698819
Dean Roos <tug91912@temple.edu>
2024-11-21 19:46:23 -0500

    Changed to modal for confirmation

--- whiteboardflow-frontend/craco.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    devServer: {
-        setupMiddlewares: (middlewares, devServer) => {
-            // Add any custom middleware or configuration if necessary
-            return middlewares;
-        },
-    },
-};
