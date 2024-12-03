d068ef33f50a322e04e1f2b686038e6053b27bff
bstephe9 <tun78906@temple.edu>
2024-12-03 03:17:43 -0500

    Add 'Unsupported Browser' page for Firefox

--- whiteboardflow-frontend/src/Main.js
+++ whiteboardflow-frontend/src/Main.js
@@ -1,4 +1,3 @@
-// Main.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
@@ -6,16 +5,23 @@ import HomePage from './components/HomePage/HomePage';
 import Whiteboard from './components/Whiteboard/Whiteboard';
 import OralTest from './components/OralTest/OralTest';
 import Results from './components/Result/Result';
-import DifficultySelect from './components/DifficultySelect/DifficultySelect';
-import QuestionSelect from './components/QuestionSelect/QuestionSelect'
+import QuestionSelect from './components/QuestionSelect/QuestionSelect';
 import { auth } from './firebase';
 import { AvatarProvider } from './components/Avatar/AvatarContext';
 import { QuestionProvider } from './components/QuestionSelect/QuestionContext';
+import UnsupportedBrowser from './components/UnsupportedBrowser/UnsupportedBrowser';
 
 function Main() {
     const [user, setUser] = useState(null);
+    const [isFirefox, setIsFirefox] = useState(false);
+    const [isBrowserChecked, setIsBrowserChecked] = useState(false);
 
     useEffect(() => {
+        // Detect Firefox browser
+        const userAgent = navigator.userAgent.toLowerCase();
+        setIsFirefox(userAgent.includes("firefox"));
+        setIsBrowserChecked(true);
+
         const unsubscribe = auth.onAuthStateChanged((user) => {
             setUser(user);
         });
@@ -27,6 +33,18 @@ function Main() {
         return user ? children : <Navigate to="/" />;
     };
 
+    // Show empty layout to avoid initial page flicker
+    if (!isBrowserChecked) {
+        return null;
+    }
+
+    // Render a message for unsupported browsers (basically, just FireFox)
+    if (isFirefox) {
+        return (
+            <UnsupportedBrowser />
+        );
+    }
+
     return (
         <Router>
             <AvatarProvider>
@@ -35,11 +53,10 @@ function Main() {
                         <Route element={<Layout user={user} />}>
                             <Route index element={<HomePage user={user} />} />
                             {/* Only allow access to these routes if user is logged in */}
-                            <Route path="OralTest" element={<PrivateRoute><OralTest /></PrivateRoute>} />
-                            <Route path="results" element={<PrivateRoute><Results /></PrivateRoute>} />
-                            <Route path="difficulty" element={<PrivateRoute><DifficultySelect /></PrivateRoute>} />
                             <Route path="questionSelect" element={<PrivateRoute><QuestionSelect /></PrivateRoute>} />
                             <Route path="whiteboard" element={<PrivateRoute><Whiteboard /></PrivateRoute>} />
+                            <Route path="oralTest" element={<PrivateRoute><OralTest /></PrivateRoute>} />
+                            <Route path="results" element={<PrivateRoute><Results /></PrivateRoute>} />
                         </Route>
                     </Routes>
                 </QuestionProvider>
